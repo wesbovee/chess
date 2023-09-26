1b6659e39b3f033fe09cf3c2897a5d69010e67e1
wes <wes.bovee@gmailcom>
2023-09-26 09:08:35 -0600

    finish setup

---
+++ src/main/chess/BoardImpl.java
@@ -0,0 +1,18 @@
+package chess;
+
+public class BoardImpl implements ChessBoard{
+    @Override
+    public void addPiece(ChessPosition position, ChessPiece piece) {
+
+    }
+
+    @Override
+    public ChessPiece getPiece(ChessPosition position) {
+        return null;
+    }
+
+    @Override
+    public void resetBoard() {
+
+    }
+}
---
+++ src/main/chess/PieceImpl.java
@@ -0,0 +1,20 @@
+package chess;
+
+import java.util.Collection;
+
+public class PieceImpl implements ChessPiece{
+    @Override
+    public ChessGame.TeamColor getTeamColor() {
+        return null;
+    }
+
+    @Override
+    public PieceType getPieceType() {
+        return null;
+    }
+
+    @Override
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+        return null;
+    }
+}
